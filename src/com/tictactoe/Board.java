21f9563a7b3fa523799f28b36740458b73feeb78
Pooja <poojag@thoughtworks.com>
2015-03-20 23:06:22 +0530

    TicTacToe repository created

---
+++ src/com/tictactoe/Board.java
@@ -0,0 +1,11 @@
+package com.tictactoe;
+
+import javax.swing.*;
+
+public class Board {
+    JFrame frame = new JFrame("game");
+    private void setProperties(){
+        frame.setSize(600,600);
+        frame.setVisible(true);
+    }
+}
